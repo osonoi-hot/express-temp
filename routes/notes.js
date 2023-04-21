2668f156f8ef0d75055c202e49fce23b56f6d369
osonoi <osonoi@hotmail.com>
2023-04-21 15:05:04 +0900

    back to 8-1

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://matsunami2:banana1208@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
